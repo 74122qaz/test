994a22c81e1871471921a3ed038b873d9b5f6596
74122qaz <74400613+74122qaz@users.noreply.github.com>
2020-11-13 16:17:42 +0800

    Update blocks.js

--- myBlocks/blockly/blocks.js
+++ myBlocks/blockly/blocks.js
@@ -1,36 +1,10 @@
-Blockly.Blocks['digitalwrite'] = {
+Blockly.Blocks['block_type'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField(Blockly.Msg.digitalWrite);
-    this.appendValueInput("pin")
-        .setCheck("Number")
-        .appendField("(");
-    this.appendDummyInput()
-        .appendField(",")
-        .appendField(new Blockly.FieldDropdown([["HIGH","1"], ["LOW","0"]]), "mode")
-        .appendField(")");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(255);
-    this.setTooltip("digitalWrite(pin, value)");
-    this.setHelpUrl("https://www.arduino.cc/reference/en/language/functions/digital-io/digitalwrite/");
-  }
-};
-
-Blockly.Blocks['digitalread'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.digitalRead);
-    this.appendValueInput("pin")
-        .setCheck("Number")
-        .appendField("(");
-    this.appendDummyInput()
-        .appendField(")");
-    this.setInputsInline(true);
-    this.setOutput(true, "Number");
-    this.setColour(60);
-    this.setTooltip("digitalRead(pin)");
-    this.setHelpUrl("https://www.arduino.cc/reference/en/language/functions/digital-io/digitalread/");
+        .appendField("哈囉");
+    this.setOutput(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
   }
 };
